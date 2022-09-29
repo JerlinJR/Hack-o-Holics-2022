edd93336c4b7209fc037e060e1209b8db7665b62
JerlinJR <infodotjerlin@gmail.com>
2022-09-29 22:47:37 +0530

    ROad Depth Insertation

--- client/client-dash.php
+++ client/client-dash.php
@@ -2,98 +2,123 @@
 
 include '../libs/load.php';
 
+
 print_r($_POST);
+
+$arr = array_values($_POST);
+
+
+
+if(!empty(isset($arr[0])) and !empty(isset($arr[1])) and !empty(isset($arr[2])) and !empty(isset($arr[3]))){
+
+	$id = $arr[0];
+	$roadType = $arr[1];
+	$roadName = $arr[2];
+	$depth = $arr[3];
+
+	
+	print(User::roadUpdate($id,$roadType,$roadName,$depth));
+} else {
+	print("Please fill the form");
+}
+
+
+
+
+
+// $result = User::roadUpdate($id)
+
+// $result = false;
 
 // if (!empty(isset($_POST['id'])) and !empty(isset($_POST['password']))) {
 //     $id = $_POST['id'];
 //     $password = $_POST['password'];
 
-//     $result = true;
+//     $result = User::userLogin($id, $password);
 // }
+
+// if ($result) {
 ?>
-<!DOCTYPE html>
-<html lang="en">
+	<!DOCTYPE html>
+	<html lang="en">
 
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>XCoders</title>
-	<link rel="stylesheet" type="text/css" href="../css/style.css">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-		integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-</head>
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>XCoders</title>
+		<link rel="stylesheet" type="text/css" href="../css/style.css">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT"
+		 crossorigin="anonymous">
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	</head>
 
-<body>
+	<body>
 
-	<div class="c-dash">
-		<div style="text-align:center" class="road-type">
-			<h2 id="heading-text">Select the type of Road</h2>
-			<form method="post" action="client-dash.php">
-				<input type="text" name="id" id="id" placeholder="Your ID" required />
-				<br>
+		<div class="c-dash">
+			<div style="text-align:center" class="road-type">
+				<h2 id="heading-text">Select the type of Road</h2>
+				<form method="post" action="client-dash.php">
+					<input type="text" name="id" id="id" placeholder="Your ID" required />
+					<br>
 
-				<select name="roads" id="roads" required>
-					<option value="">None</option>
-					<option value="stateHighway">State Highway</option>
-					<option value="district">District Roads</option>
-					<option value="villageRoad">Village Roads</option>
-				</select>
-				<br>
-				<!-- state highway -->
-
-				<div id="stateHighway">
-					<select id="exp" name="stateHighwayRoads">
-						<option selected="true" disabled="disabled">Choose Tagging</option>
-						<option value="SH1">SH1</option>
-						<option value="SH2">SH2</option>
-						<option value="SH3">SH3</option>
-						<option value="SH4">SH4</option>
+					<select name="roads" id="roads" required>
+						<option value="">None</option>
+						<option value="stateHighway">State Highway</option>
+						<option value="district">District Roads</option>
+						<option value="villageRoad">Village Roads</option>
 					</select>
-				</div>
+					<br>
+					<!-- state highway -->
+
+					<div id="stateHighway">
+						<select id="exp" name="stateHighwayRoads">
+							<option selected="true" disabled="disabled">Choose Tagging</option>
+							<option value="SH1">SH1</option>
+							<option value="SH2">SH2</option>
+							<option value="SH3">SH3</option>
+							<option value="SH4">SH4</option>
+						</select>
+					</div>
 
 
-				<!-- District -->
+					<!-- District -->
 
-				<div id="district">
-					<select id="team" name="districtRoads">
-						<option selected="true" disabled="disabled">Choose Tagging</option>
-						<option value="DR1">DR1</option>
-						<option value="DR2">DR2</option>
-						<option value="DR3">DR3</option>
-						<option value="DR4">DR4</option>
-						<option value="DR5">DR5</option>
-						<option value="DR6">DR6</option>
-					</select>
-				</div>
+					<div id="district">
+						<select id="team" name="districtRoads">
+							<option selected="true" disabled="disabled">Choose Tagging</option>
+							<option value="DR1">DR1</option>
+							<option value="DR2">DR2</option>
+							<option value="DR3">DR3</option>
+							<option value="DR4">DR4</option>
+							<option value="DR5">DR5</option>
+							<option value="DR6">DR6</option>
+						</select>
+					</div>
 
 
-				<!-- Village Road -->
+					<!-- Village Road -->
 
-				<div id="villageRoad">
-					<select id="age" name="villageRoad">
-						<option selected="true" disabled="disabled">Choose Tagging</option>
-						<option value="VR1">VR1</option>
-						<option value="VR2">VR2</option>
-						<option value="VR3">VR3</option>
-						<option value="VR4">VR4</option>
-					</select>
-				</div>
+					<div id="villageRoad">
+						<select id="age" name="villageRoad">
+							<option selected="true" disabled="disabled">Choose Tagging</option>
+							<option value="VR1">VR1</option>
+							<option value="VR2">VR2</option>
+							<option value="VR3">VR3</option>
+							<option value="VR4">VR4</option>
+						</select>
+					</div>
 
-				<input type="number" name="damageDepth" id="damage" placeholder="Damage Depth in feet" required />
-				<br>
-				<br>
-				<input type="submit" class="btn btn-primary">
-			</form>
+					<input type="number" name="damageDepth" id="damage" placeholder="Damage Depth in feet" required />
+					<br>
+					<br>
+					<input type="submit" class="btn btn-primary">
+				</form>
+			</div>
 		</div>
-	</div>
 
 
-</body>
-<script src="script.js"></script>
+	</body>
+	<script src="script.js"></script>
 
-</html>
-
-<!-- Array ( [id] => 1 [roads] => stateHighway [exp] => 1 [team] => 1 [age] => 1 [damageDepth] => 2 )
- -->
+	</html>
--- libs/includes/User.class.php
+++ libs/includes/User.class.php
@@ -4,16 +4,16 @@ class User
 {
     private $conn;
 
-    public static function login($user, $pass)
+    public static function userLogin($user, $pass)
     {
         $conn = Database::getConnection();
 
-        $sql = "SELECT * FROM `clientAuth` WHERE `username` = '$user' ";
+        $sql = "SELECT * FROM `clientauth` WHERE `username` = '$user' ";
         $result = $conn->query($sql);
         if ($result->num_rows == 1) {
             $row = $result->fetch_assoc();
             if ($pass == $row['password']) {
-                return $row['email'];
+                return $row['id'];
             } else {
                 return false;
             }
@@ -21,6 +21,7 @@ class User
             return false;
         }
     }
+
     public static function moderatorLogin($user, $pass)
     {
         $conn = Database::getConnection();
@@ -53,17 +54,30 @@ class User
         }
     }
 
-    public static function priceUpdate($cement,$mcent,$agrigator,$labourcharge,$bitumin,$layingCharge,$mixingCharge )
+    public static function priceUpdate($cement, $mcent, $agrigator, $labourcharge, $bitumin, $layingCharge, $mixingCharge)
     {
         $conn = Database::getConnection();
 
         $sql = "UPDATE `materialprice` SET `cement`= '$cement',`mcent`='$mcent',`agrigator`='$agrigator',`labourcharge`='$labourcharge',`bitumin`='$bitumin',`laying`='$layingCharge',`mixing`='$mixingCharge'  WHERE `id`= '1'";
-        if ($conn->query($sql) === TRUE) {
-            return True;
+        if ($conn->query($sql) === true) {
+            return true;
         } else {
             // echo "Error updating record: " . $conn->error;
-            return False;
+            return false;
         }
     }
 
+    public static function roadUpdate($id, $roadType, $roadName, $depth)
+    {
+        $conn = Database::getConnection();
+
+        $sql = "UPDATE `clientauth` SET `roadType`='$roadType',`roadName`='$roadName',`depth`='$depth' WHERE `id`='$id'";
+        if ($conn->query($sql) === true) {
+            echo "Data Inserted";
+        // return true;
+        } else {
+            echo "Error updating record: " . $conn->error;
+            // return false;
+        }
+    }
 }
